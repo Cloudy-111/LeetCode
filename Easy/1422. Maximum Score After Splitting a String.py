3b8ea965faa3579a8585482569f0bdac2dfe854f
Diep <diepani303@gmail.com>
2025-01-01 12:33:45 +0700

    Commit

--- Easy/1422. Maximum Score After Splitting a String.py
+++ Easy/1422. Maximum Score After Splitting a String.py
@@ -1,15 +1,13 @@
 class Solution:
-    # nếu cả chuối là 1 số thì chỉ cần trả về độ dài - 1
-    # vì đề yêu cầu không chia xâu rỗng nên không duyệt đến hết xâu
     def maxScore(self, s: str) -> int:
-        if len(set(list(s))) == 1:
-            return len(s) - 1
-        left, right = 0, s.count('1')
-        res = 0
-        for i in range(len(s) - 1):
-            if s[i] == '0':
-                left += 1
+        left = 1 if s[0] == '0' else 0
+        right = s[1:].count('1')
+        res = left + right
+        for i in range(1, len(s) - 1):
             if s[i] == '1':
                 right -= 1
-            res = max(res, left + right)
+                res = max(res, left + right)
+            else:
+                left += 1
+                res = max(res, left + right)
         return res
